8109d0c2b8c99941dec5996fb6323e4f56704a65
mnzube <mnzube@gmail.com>
2019-03-06 22:19:19 +0300

    [Feature #164394519]enables get messages

---
+++ SERVER/models/messages.js
@@ -0,0 +1,18 @@
+class Message {
+  /**
+   * class constructor
+   * @param {object} data
+   */
+  constructor() {
+    this.messages = [];
+  }
+ 
+  /**
+   * @returns {object} returns all messages
+   */
+  findAll() {
+    return this.messages;
+  }
+  
+}
+export default new Message();
