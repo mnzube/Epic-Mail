c04af317dcf8f67c197cbd144d7fe9f63ad0764d
mnzube <mnzube@gmail.com>
2019-03-18 14:41:56 +0200

    created db setup file

---
+++ SERVER/db/db.js
@@ -0,0 +1,11 @@
+import { pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+pool.on('connect', () => {
+    console.log('connected to the db');
+});
+
+export default pool;
