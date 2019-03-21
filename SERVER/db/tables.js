f446825f6a75bb019ee95a636dd1b83e0cc7137b
mnzube <mnzube@gmail.com>
2019-03-21 13:04:37 +0300

    [Feature#164770751]added drop tables function

--- SERVER/db/tables.js
+++ SERVER/db/tables.js
@@ -1,7 +1,7 @@
 import db from './db';
 
-const createTable =()=>{
-const messages =
+const createTable =()=>new Promise ((resolve,reject)=>{
+const tables =
  `CREATE TABLE IF NOT EXISTS
  messages(
      message_id serial PRIMARY KEY NOT NULL,
@@ -9,9 +9,8 @@ const messages =
      sender_id INTEGER NOT NULL,
      receiver_id INTEGER NOT NULL,
      status VARCHAR(100)NOT NULL
- )`;
- const user = 
- `CREATE TABLE IF NOT EXISTS
+ );
+ CREATE TABLE IF NOT EXISTS
  users(
      user_id serial PRIMARY KEY NOT NULL,
      firstname VARCHAR(100)NOT NULL,
@@ -19,64 +18,51 @@ const messages =
      email VARCHAR(100) NOT NULL,
      password VARCHAR(100)NOT NULL,
      isadmin BOOLEAN DEFAULT false NOT NULL
- )`;
- const sentmail = 
- `CREATE TABLE IF NOT EXISTS
+ );
+ CREATE TABLE IF NOT EXISTS
  sentmail(
      sent_id serial PRIMARY KEY NOT NULL,
      user_id INTEGER NOT NULL,
-     message_id INTEGER NOT NULL)`;
+     message_id INTEGER NOT NULL);
 
-const groupMessage= 
- `CREATE TABLE IF NOT EXISTS
+  CREATE TABLE IF NOT EXISTS
  groupmail(
      group_id serial PRIMARY KEY NOT NULL,
      group_name VARCHAR(100) NOT NULL,
      members INTEGER NOT NULL,
-     admin BOOLEAN DEFAULT false)`;
- //messages
- db.query(messages)
+     admin BOOLEAN DEFAULT false);`
+
+ db.query(tables)
  .then((res)=> {
-     console.log(res);
-     db.end();
+     //console.log(res);
+     resolve();
+  
+     
  })
  .catch((error)=>{
-     console.log(error);
+    // console.log(error);
+     reject(error);
  })
- //users 
- db.query(user)
- .then((res) =>{
- console.log(res);
- db.end();
-})
-.catch((error)=>{
-console.log(error);
-})
- //sent mail 
- db.query(sentmail)
- .then((res) =>{
- console.log(res);
- db.end();
-})
-.catch((error)=>{
-console.log(error);
-})
- //groups 
- db.query(groupMessage)
- .then((res) =>{
- console.log(res);
- db.end();
-})
-.catch((error)=>{
-console.log(error);
-})
-}
-db.on('remove',() => {
-    console.log('client removed');
-    process.exit(0);
 });
- module.exports={
-     createTable
- }
+const dropTable =()=>new Promise ((resolve,reject)=>{
+    const tables = 
+    `DROP TABLE IF EXISTS messages CASCADE;
+     DROP TABLE IF EXISTS users CASCADE;
+     DROP TABLE IF EXISTS sentmail CASCADE;
+     DROP TABLE IF EXISTS groupmail CASCADE;
+    `;
+    db.query(tables)
+ .then((res) =>{
+ //console.log(res);
+ resolve();
+})
+.catch((error)=>{
+//console.log(error);
+reject(error);
+});
+});
+
+
+ export{createTable, dropTable};
 
  require('make-runnable');
